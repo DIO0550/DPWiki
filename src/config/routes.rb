ddee2fa09413c6da291fa5496abb6da7497a2cbb
DIO0550 <diot12345@yahoo.co.jp>
2020-02-21 23:07:00 +0900

    モンスター情報のルーティング追加。

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      get '/pmonsters', to: 'pmonsters#index' 
+    end
+  end
+  root to: 'api/v1/pmonsters#index'
 end
