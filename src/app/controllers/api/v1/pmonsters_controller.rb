fd785439eade8f60d73f352e46bac59992c16bc4
DIO0550 <diot12345@yahoo.co.jp>
2020-02-22 12:17:08 +0900

    ネストする必要がないので、ネストなしに修正。

--- src/app/controllers/api/v1/pmonsters_controller.rb
+++ src/app/controllers/api/v1/pmonsters_controller.rb
@@ -1,12 +1,8 @@
 require "#{Rails.root}/app/controllers/application_controller.rb"
 
-module Api
-    module V1
-        class PmonstersController < ApplicationController
-            def index
-                @pmonsters = Pmonster.all
-                render json: @pmonsters
-            end
-        end
+class Api::V1::PmonstersController < ApplicationController
+    def index
+        @pmonsters = Pmonster.all
+        render json: @pmonsters
     end
 end
