f29133d674256c2a07e27b2c570aca3a49ee06bf
DIO0550 <diot12345@yahoo.co.jp>
2020-02-22 12:59:16 +0900

    ヘッダーとフッターの読み込み処理を作成。

--- src/frontend/component/App.jsx
+++ src/frontend/component/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 export default class App extends React.Component {
   render () {
-    return <p>Hello React!</p>;
+    return (
+      <div>
+        <Header />
+        <p>PMonster!</p>
+        <Footer />
+      </div>
+    )
   }
 }
