23073ac7db1572329cc94de5a58e296bb809a26a
DIO0550 <diot12345@yahoo.co.jp>
2020-02-22 12:58:24 +0900

    フッター用のコンポーネントを作成

---
+++ src/frontend/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+          ヘッダーです
+      </div>
+    );
+  }
+}
