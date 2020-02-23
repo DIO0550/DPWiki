a390f8878e014c9b440de4e2a80c45edc739f16d
DIO0550 <diot12345@yahoo.co.jp>
2020-02-23 23:12:28 +0900

    アプリケーションの定数ファイル作成。

---
+++ src/frontend/js/app-const.jsx
@@ -0,0 +1,11 @@
+
+const APP_CONST = {
+    PMONSTERS_LIST_API: "http://localhost:3000/api/v1/pmonsters",
+    API_CALL_STATUS: {
+        NOT_CALL: 0,
+        SUCCESS: 1,
+        FAILED: 2
+    }
+}
+
+export default APP_CONST;
