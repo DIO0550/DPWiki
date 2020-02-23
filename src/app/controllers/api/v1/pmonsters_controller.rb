e081ea98906322539a8e8f2a2f951fcc87705dad
DIO0550 <diot12345@yahoo.co.jp>
2020-02-23 17:19:13 +0900

    joinした結果を返すように修正。

--- src/app/controllers/api/v1/pmonsters_controller.rb
+++ src/app/controllers/api/v1/pmonsters_controller.rb
@@ -2,7 +2,8 @@ require "#{Rails.root}/app/controllers/application_controller.rb"
 
 class Api::V1::PmonstersController < ApplicationController
     def index
-        @pmonsters = Pmonster.all
+        select_column = "type1s.type1_name, type2s.type2_name, pmonsters.number, pmonsters.monster_name, pmonsters.hit_point, pmonsters.attack, pmonsters.defence, pmonsters.special_attack, pmonsters.special_defence, pmonsters.speed"
+        @pmonsters = Pmonster.joins(:type1).joins(:type2).select(select_column)
         render json: @pmonsters
     end
 end
