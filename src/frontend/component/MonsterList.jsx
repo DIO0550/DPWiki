6f4283b6c0123b0572f922cbb3ba6958e06928df
DIO0550 <diot12345@yahoo.co.jp>
2020-02-23 23:11:59 +0900

    モンスターの一覧を表示するコンポーネント作成。

---
+++ src/frontend/component/MonsterList.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import axios from 'axios';
+import APP_CONST from '../js/app-const.jsx';
+
+export default class MonsterList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mosnter_list: {},
+      api_call_status: APP_CONST.API_CALL_STATUS.NOT_CALL
+    }
+  }
+  componentDidMount() {
+    axios.get(APP_CONST.PMONSTERS_LIST_API, { withCredentials: true })
+    .then((response) => {
+        console.debug(response, "ressponse");
+        this.state.mosnter_list = response.data
+        this.setState({
+            mosnter_list: response.data,
+            api_call_status: APP_CONST.API_CALL_STATUS.SUCCESS
+        })
+    }).catch((error) => {
+        console.debug(error);
+        this.setState({
+            api_call_status: APP_CONST.API_CALL_STATUS.FAILED
+        })
+    });
+  }
+  pmonsterList(api_call_status) {
+      if (api_call_status == APP_CONST.API_CALL_STATUS.NOT_CALL) {
+          return <span>一覧読み込み中</span>
+      } else if (api_call_status == APP_CONST.API_CALL_STATUS.SUCCESS) {
+          var list = [];
+
+          for(var i in this.state.mosnter_list){
+            list.push(
+                <tr>
+                    <th>{this.state.mosnter_list[i].number}</th>
+                    <th>{this.state.mosnter_list[i].monster_name}</th>
+                    <th>{this.state.mosnter_list[i].type1_name}</th>
+                    <th>{this.state.mosnter_list[i].type2_name}</th>
+                    <th>{this.state.mosnter_list[i].hit_point}</th>
+                    <th>{this.state.mosnter_list[i].attack}</th>
+                    <th>{this.state.mosnter_list[i].defence}</th>
+                    <th>{this.state.mosnter_list[i].special_attack}</th>
+                    <th>{this.state.mosnter_list[i].special_defence}</th>
+                    <th>{this.state.mosnter_list[i].speed}</th>
+                </tr>
+            )}
+          return (
+              <table border="1">
+            <tr>
+                <th>図鑑番号</th>
+                <th>名前</th>
+                <th>タイプ１</th>
+                <th>タイプ２</th>
+                <th>HP</th>
+                <th>攻撃</th>
+                <th>防御</th>
+                <th>特攻</th>
+                <th>特防</th>
+                <th>素早さ</th>
+            </tr>
+            { list }
+            </table>
+          )
+      } else {
+          return <span>一覧取得に失敗しました。</span>
+      }
+  }
+  render() {
+
+    return (
+      <div className="list-contents">
+          { this.pmonsterList(this.state.api_call_status) }
+      </div>
+    );
+  }
+}
