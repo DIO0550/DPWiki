4c65013461bfacc7a34c3783d463c8308cf0609b
DIO0550 <diot12345@yahoo.co.jp>
2020-02-25 22:35:46 +0900

    フッターになっていた箇所をヘッダーに修正。

--- src/frontend/component/Header.jsx
+++ src/frontend/component/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (
-            <div className="footer">
-                フッターです
+            <div className="Header">
+                PMONSTERS
             </div>
         );
     }
