339d6113fc732cdeb86091010784c8024692c57b
DIO0550 <diot12345@yahoo.co.jp>
2020-02-25 22:36:22 +0900

    フッターにスタイル適用する処理を追加。

--- src/frontend/component/Footer.jsx
+++ src/frontend/component/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from '../stylesheet/footer.sass';
 
-export default class Header extends React.Component {
+export default class Footer extends React.Component {
+  
   render() {
     return (
-      <div className="header">
-          ヘッダーです
+      <div className={styles.footer}>
+          Copyright (c) 2020 DIO0550
       </div>
     );
   }
 }
+
