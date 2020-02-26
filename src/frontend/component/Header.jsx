2a45d994b885a52f2ef2fd038f3ca990fa3d78cc
DIO0550 <diot12345@yahoo.co.jp>
2020-02-26 22:45:08 +0900

    headerにsassのimport追加。

--- src/frontend/component/Header.jsx
+++ src/frontend/component/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../stylesheet/header.sass';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div className="Header">
+            <div className={styles.header}>
                 PMONSTERS
             </div>
         );
