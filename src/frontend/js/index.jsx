33f5c37d6da2bc6c11a6c949726623b930dedc0d
DIO0550 <diot12345@yahoo.co.jp>
2020-02-27 22:27:01 +0900

    reduxの設定を追加。

--- src/frontend/js/index.jsx
+++ src/frontend/js/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../component/App.jsx';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import reducer from './reducer.jsx'
+
+const store = createStore(reducer)
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('app')
 );
