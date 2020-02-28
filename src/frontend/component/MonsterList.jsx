a01eaa61980d5345904c9a99fe23e9e9cfd9605a
DIO0550 <diot12345@yahoo.co.jp>
2020-02-29 00:06:34 +0900

    図鑑番号と名前のみ、クリックで以下を実行。 ソートの変更 ヘッダーの横にソートされていることを示すマークを表示。

--- src/frontend/component/MonsterList.jsx
+++ src/frontend/component/MonsterList.jsx
@@ -4,14 +4,23 @@ import APP_CONST from '../js/app-const.jsx';
 import * as monster_sort from '../js/monster-sort.jsx';
 import '../stylesheet/monster-list.sass';
 
+const ASCENDING_SORT_MARK = " ▲"
+const DESCENDING_SORT_MARK = " ▼"
+
 export default class MonsterList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       mosnter_list: {},
-      api_call_status: APP_CONST.API_CALL_STATUS.NOT_CALL
+      api_call_status: APP_CONST.API_CALL_STATUS.NOT_CALL,
+      sort_type: monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NUMBER
     }
+    this.onClickNumberHeader = this.onClickNumberHeader.bind(this)
+    this.onClickNameHeader = this.onClickNameHeader.bind(this)
   }
+  /**
+   * マウント後
+   */
   componentDidMount() {
     axios.get(APP_CONST.PMONSTERS_LIST_API, { withCredentials: true })
     .then((response) => {
@@ -28,13 +37,97 @@ export default class MonsterList extends React.Component {
         })
     });
   }
-  pmonsterList(api_call_status) {
-      if (api_call_status == APP_CONST.API_CALL_STATUS.NOT_CALL) {
+
+  /**
+   * 図鑑番号ヘッダークリック時の処理
+   */
+  onClickNumberHeader(e) {
+    console.log("onClickNumberHeader")
+    if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NUMBER) {
+      this.setState({
+        sort_type:monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NUMBER
+      })
+      return
+    }
+    this.setState({
+      sort_type:monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NUMBER
+    })
+  }
+
+  /**
+   * 名前ヘッダークリック時の処理
+   */
+  onClickNameHeader(e) {
+    console.log("onClickNumberHeader")
+    if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NAME) {
+      this.setState({
+        sort_type:monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NAME
+      })
+      return
+    }
+    this.setState({
+      sort_type:monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NAME
+    })
+  }
+
+  sortFunction() {
+    // 図鑑番号 - 昇順
+    if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NUMBER) {
+      return monster_sort.monsterAscendingSortNumber
+    } 
+    // 図鑑番号 - 降順
+    else if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NUMBER) {
+      return monster_sort.monsterDescendingSortNumber
+    }
+    // 名前 - 昇順
+    else if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NAME) {
+      return monster_sort.monsterAscendingSortName
+    }
+    // 名前 - 降順
+    else if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NAME) {
+      return monster_sort.monsterDescendingSortName
+    }
+  }
+
+  /**
+   * 図鑑番号のヘッダーのソート文字列
+   */
+  numberHaderSortMark() {
+    if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NUMBER) {
+      return ASCENDING_SORT_MARK
+    } 
+    // 図鑑番号 - 降順
+    else if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NUMBER) {
+      return DESCENDING_SORT_MARK
+    }
+    return ""
+  }
+
+  /**
+   * 名前のヘッダーのソート文字列
+   */
+  nameHaderSortMark() {
+    if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.ASCENDING_SORT_NAME) {
+      return ASCENDING_SORT_MARK
+    } 
+    // 図鑑番号 - 降順
+    else if (this.state.sort_type == monster_sort.MONSTER_SORT_TYPE.DESCENDING_SORT_NAME) {
+      return DESCENDING_SORT_MARK
+    }
+    return ""
+  }
+
+  /**
+   * List部分
+   */
+  pmonsterList() {
+      if (this.state.api_call_status == APP_CONST.API_CALL_STATUS.NOT_CALL) {
           return <span>一覧読み込み中</span>
-      } else if (api_call_status == APP_CONST.API_CALL_STATUS.SUCCESS) {
+      } else if (this.state.api_call_status == APP_CONST.API_CALL_STATUS.SUCCESS) {
           var list = [];
+          let sort_function = this.sortFunction() 
           this.state.mosnter_list.sort(function(monster1, monster2){
-                return monster_sort.monsterAscendingSortNumber(monster1, monster2)
+                return sort_function(monster1, monster2)
           });
           for(var i in this.state.mosnter_list){
             list.push(
@@ -57,8 +150,12 @@ export default class MonsterList extends React.Component {
               <table border="5">
                  <thead>
                   <tr>
-                    <th>図鑑番号</th>
-                    <th>名前</th>
+                    <th onClick={this.onClickNumberHeader}>
+                      図鑑番号 { this.numberHaderSortMark() }
+                    </th>
+                    <th onClick={this.onClickNameHeader}>
+                      名前 { this.nameHaderSortMark() }
+                    </th>
                     <th>タイプ１</th>
                     <th>タイプ２</th>
                     <th>HP</th>
@@ -80,7 +177,7 @@ export default class MonsterList extends React.Component {
 
     return (
       <div className="list-contents">
-          { this.pmonsterList(this.state.api_call_status) }
+          { this.pmonsterList() }
       </div>
     );
   }
