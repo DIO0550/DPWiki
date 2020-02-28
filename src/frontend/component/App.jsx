9dbb387374f1a62ee680628ea2cd1badc6028ce6
DIO0550 <diot12345@yahoo.co.jp>
2020-02-28 22:52:44 +0900

    sassを適用。

--- src/frontend/component/App.jsx
+++ src/frontend/component/App.jsx
@@ -5,11 +5,12 @@ import Footer from './Footer.jsx';
 import TopPage from './TopPage.jsx';
 import MonsterList from './MonsterList.jsx';
 import TabMenu from './TabMenu.jsx';
+import styles from '../stylesheet/app.sass';
 
 export default class App extends React.Component {
   render () {
     return (
-      <div>
+      <div className={styles['app-container']}>
         <Header />
         <TabMenu />
         <Router>
