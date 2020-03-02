69228c4264ddb41b7e054a3b45ce8f9625c602a7
DIO0550 <diot12345@yahoo.co.jp>
2020-03-02 22:40:14 +0900

    パスから選択中のタブの色を変える処理を追加。

--- src/frontend/component/TabMenu.jsx
+++ src/frontend/component/TabMenu.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
 import styles from '../stylesheet/tab-menu.sass';
+import APP_CONST from '../js/app-const.jsx';
+import classNames from 'classnames';
+
+// パス名
+const path_name = window.location.pathname;
 
 export default class TabMenu extends React.Component {
+
+    /**
+     * トップページのクラス名
+     */
+    topPageClassNames() {
+        var className = classNames(styles['tab-menu'], {
+            [styles['tab-select']] : path_name == APP_CONST.ROUTER_PATH.TOP_PAGE
+        })
+        return className
+    }
+
+    /**
+     * 一覧のクラス名
+     */
+    listClassNames() {
+        var className = classNames(styles['tab-menu'], {
+            [styles['tab-select']] : path_name == APP_CONST.ROUTER_PATH.LIST
+        })
+        return className
+    }
+
+    /**
+     * 相性表のクラス名
+     */
+    compatibilityTableClassNames() {
+
+    }
+
+    /**
+     * チーム編成のクラス名
+     */
+    teamFormationClassNames() {
+
+    }
+
     render() {
         return (
             <div className={styles['tab-menus']}>
-                <div className={styles['tab-menu']}>
+                <div className={this.topPageClassNames()}>
                     トップページ
                 </div>
-                <div className={styles['tab-menu']}>
+                <div className={this.listClassNames()}>
                     一覧
                 </div>
                 <div className={styles['tab-menu']}>
