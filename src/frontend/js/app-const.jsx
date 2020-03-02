a0250ead9baa02a08b444ab171d0fe54bd607ba1
DIO0550 <diot12345@yahoo.co.jp>
2020-03-02 22:40:27 +0900

    ルーティングのパスを追加。

--- src/frontend/js/app-const.jsx
+++ src/frontend/js/app-const.jsx
@@ -1,10 +1,25 @@
 
 const APP_CONST = {
+    
+    /**
+     * モンスター一覧のAPIのパス
+     */
     PMONSTERS_LIST_API: "http://localhost:3000/api/v1/pmonsters",
+    
+    /**
+     * APIの呼び出しステータス
+     */
     API_CALL_STATUS: {
         NOT_CALL: 0,
         SUCCESS: 1,
         FAILED: 2
+    },
+
+    ROUTER_PATH: {
+        TOP_PAGE: "/",
+        LIST: "/List",
+        COMPATIBILITY_TABLE: "/CompatibilityTable",
+        TEAM_FORMATION: "/TeamFormation"
     }
 }
 
