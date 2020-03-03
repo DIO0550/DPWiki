efaa1d9f0ff29943b4e75878272f1253f0790ed8
DIO0550 <diot12345@yahoo.co.jp>
2020-03-03 23:36:45 +0900

    タブクリックでページを遷移&タブの選択切り替え実行。

--- src/frontend/component/TabMenu.jsx
+++ src/frontend/component/TabMenu.jsx
@@ -6,23 +6,37 @@ import classNames from 'classnames';
 import { withRouter, Link } from 'react-router-dom';
 
 // パス名
-const path_name = window.location.pathname;
+var path_name = window.location.pathname;
 
 class TabMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.handleToListPage = this.handleToListPage.bind(this)
+        this.state = {
+            route_path: path_name
+        }
+        this.pushToListPage = this.pushToListPage.bind(this)
+        this.pushToTopPage = this.pushToTopPage.bind(this)
+        this.listClassNames = this.listClassNames.bind(this)
+        this.topPageClassNames = this.topPageClassNames.bind(this)
     }
-    handleToListPage(e) {
-        this.props.history.push('/List')
+    pushToTopPage(e) {
+        this.props.history.push(APP_CONST.ROUTER_PATH.TOP_PAGE)
+        this.setState({
+            route_path:window.location.pathname
+        })
+    }
+    pushToListPage(e) {
+        this.props.history.push(APP_CONST.ROUTER_PATH.LIST)
+        this.setState({
+            route_path:window.location.pathname
+        })
     } 
-
     /**
      * トップページのクラス名
      */
     topPageClassNames() {
         var className = classNames(styles['tab-menu'], {
-            [styles['tab-select']] : path_name == APP_CONST.ROUTER_PATH.TOP_PAGE
+            [styles['tab-select']] : this.state.route_path == APP_CONST.ROUTER_PATH.TOP_PAGE
         })
         return className
     }
@@ -32,7 +46,7 @@ class TabMenu extends React.Component {
      */
     listClassNames() {
         var className = classNames(styles['tab-menu'], {
-            [styles['tab-select']] : path_name == APP_CONST.ROUTER_PATH.LIST
+            [styles['tab-select']] : this.state.route_path == APP_CONST.ROUTER_PATH.LIST
         })
         return className
     }
@@ -54,12 +68,10 @@ class TabMenu extends React.Component {
     render() {
         return (
             <div className={styles['tab-menus']}>
-                <div className={this.topPageClassNames()} >
-                    <Link to={APP_CONST.ROUTER_PATH.TOP_PAGE}>
-                        トップページ
-                    </Link>
+                <div className={this.topPageClassNames()} onClick={this.pushToTopPage}>
+                    トップページ
                 </div>
-                <div className={this.listClassNames()} onClick={this.handleToListPage}>
+                <div className={this.listClassNames()} onClick={this.pushToListPage}>
                         一覧
                 </div>
                 <div className={styles['tab-menu']}>
