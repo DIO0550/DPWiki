5e5b2cb7225bce58589e117115e09e4c567b2355
DIO0550 <diot12345@yahoo.co.jp>
2020-03-04 21:53:27 +0900

    相性表のコンポーネントを追加。

---
+++ src/frontend/component/CompatibilityTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class CompatibilityTable extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        相性表
+      </div>
+    );
+  }
+}
+
