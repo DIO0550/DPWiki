5f417814e04de450d792a0b603eca48afe257add
DIO0550 <diot12345@yahoo.co.jp>
2020-03-06 23:15:10 +0900

    モデルなど調整。

--- src/db/migrate/20200305131126_create_team_formations.rb
+++ src/db/migrate/20200305131126_create_team_formations.rb
@@ -1,11 +1,11 @@
 class CreateTeamFormations < ActiveRecord::Migration[6.0]
   def change
-    create_table :team_formations do |t|
+    create_table :team_formations, id: false do |t|
       t.string :team_id, null: false
-      t.refarence :pmonster_number, null: false, foreign_key: true, type: :varchar(7)
+      t.references :number, foreign_key: { to_table: :pmonsters }, null: false, type: :string
 
       t.timestamps
     end
-    add_index :team_formations, [:team_id, :pmonster_number], unique: true
+    add_index :team_formations, [:number, :team_id], unique: true
   end
 end
