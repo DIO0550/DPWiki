59a2c893f45633ab1f40293991718687dc1d29c7
DIO0550 <diot12345@yahoo.co.jp>
2020-03-07 18:47:37 +0900

    チーム編成APIのルーティング追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get '/pmonsters', to: 'pmonsters#index' 
+
+      get '/team_formations', to: 'team_formations#index' 
     end
   end
   root to: 'api/v1/pmonsters#index'
