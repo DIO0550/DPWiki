bbe850fac4e46a45d22985c99dad34fac353d8d2
DIO0550 <diot12345@yahoo.co.jp>
2020-03-28 20:51:19 +0900

    チーム編集画面のコンポーネント追加。

---
+++ src/frontend/component/EditTeam.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class EditTeam extends React.Component {
+  
+  render() {
+    return (
+      <div>
+          EditTeam
+      </div>
+    );
+  }
+}
+
